f5f6dd1a414352c063ec812752df76e39830cdd4
Michael Isaiah <michael.isaiah.02@gmail.com>
2023-05-18 02:18:36 +0700

    Acara dan Gallery

--- script.js
+++ script.js
@@ -3,10 +3,15 @@ $(document).ready(function () {
   $("#detailUndangan").hide();
   $("#navbar").hide();
 
+  // Ketika tombol "Buka Undangan" ditekan
   $("#bukaUndangan").click(function () {
     $("#cover").fadeOut("1000", function () {
+      // Tampil Detail Undangan
       $("#detailUndangan").slideDown("1000");
       $("#navbar").slideDown("1000");
+      // Memainkan Musik
+      var audio = document.getElementById("musik");
+      audio.play();
     });
   });
 });
